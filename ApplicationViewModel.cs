d4718869995309fda0b35ea9e8413fa16347f00e
Андрей Зуев <andrej.zuev.2918@mail.ru>
2024-04-24 19:22:42 +0500

    Добавьте файлы проекта.

---
+++ ApplicationViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Паттерн_MVVM
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Рецепт selectedRecipe;
+
+        public ObservableCollection<Рецепт> Recipes { get; set; }
+        public Рецепт SelectedRecipe
+        {
+            get { return selectedRecipe; }
+            set
+            {
+                selectedRecipe = value;
+                OnPropertyChanged("SelectedRecipe");
+            }
+        }
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Рецепт recipe = new Рецепт("", Рецепт.razdel.Горячее, 0, 0, "", "", "");
+                      Recipes.Insert(0, recipe);
+                      SelectedRecipe = recipe;
+                  }));
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      Рецепт recipe = obj as Рецепт;
+                      if (recipe != null)
+                      {
+                          Recipes.Remove(recipe);
+                      }
+                  },
+                 (obj) => Recipes.Count > 0));
+            }
+        }
+
+        RelayCommand doubleCommand;
+        public RelayCommand DoubleCommand
+        {
+            get
+            {
+                return doubleCommand ??
+                    (doubleCommand = new RelayCommand(obj =>
+                    {
+                        Рецепт phone = obj as Рецепт;
+                        if (phone != null)
+                        {
+                            Рецепт phoneCopy = new Рецепт(phone.Title, phone.RecipeRazdel, phone.Price, phone.Cal, phone.MainIngridient, phone.Description, phone.Photo);
+                            Recipes.Insert(0, phoneCopy);
+                        }
+                    }));
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Recipes = new ObservableCollection<Рецепт>
+            {
+                new Рецепт("Цезарь", Рецепт.razdel.Салат, 400, 150, "Курица", "Cалат цезарь. ", "imgs/Цезарь.jpg"),
+                new Рецепт ("Сырный суп", Рецепт.razdel.Горячее, 500, 210,"Сыр", "Очень сырный суп. ", "imgs/Сырный суп.jpg" ),
+                new Рецепт ("Вителло-тоннато",Рецепт.razdel.Закуска, 300, 200, "Мясо", "Закуска вителло-тоннато.", "imgs/Что.jpg"),
+                new Рецепт ("Чизкейк", Рецепт.razdel.Десерт, 300, 250, "Сливочный сыр", "Чизкейк Нью-Йорк. ", "imgs/Чизкейк.jpg" )
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Рецепт.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Паттерн_MVVM
+{
+    public class Рецепт : INotifyPropertyChanged
+    {
+        public enum razdel
+        {
+            Горячее,
+            Десерт,
+            Закуска,
+            Салат,
+            Гарнир
+        }
+        private string title;
+        private string mainIngridient;
+        private razdel recipeRazdel;
+        private string description;
+        private double cal;
+        private double price;
+        private string photo;
+        public string Title { get { return title; }
+            set
+            {
+                // обработка изменения свойства
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public string MainIngridient {
+            get { return mainIngridient; }
+            set
+            {
+                // обработка изменения свойства
+                mainIngridient = value;
+                OnPropertyChanged("mainIngridient");
+            }
+        }
+        public razdel RecipeRazdel {
+            get { return recipeRazdel; }
+            set
+            {
+                // обработка изменения свойства
+                recipeRazdel = value;
+                OnPropertyChanged("recipeRazdel");
+            }
+        }
+        public string Description {
+            get { return description; }
+            set
+            {
+                // обработка изменения свойства
+                description = value;
+                OnPropertyChanged("description");
+            }
+        }
+        public double Cal {
+            get { return cal; }
+            set
+            {
+                // обработка изменения свойства
+                cal = value;
+                OnPropertyChanged("cal");
+            }
+        }
+        public double Price {
+            get { return price; }
+            set
+            {
+                // обработка изменения свойства
+                price = value;
+                OnPropertyChanged("price");
+            }
+        }
+        public string Photo {
+            get { return photo; }
+            set
+            {
+                // обработка изменения свойства
+                photo = value;
+                OnPropertyChanged("photo");
+            }
+        }
+
+        public Рецепт(string Title, razdel recipeRazdel, double price, double cal, string mainIngridient, string description, string photo)
+        {
+            this.Title = Title;
+            this.recipeRazdel = recipeRazdel;
+            this.price = price;
+            this.cal = cal;
+            this.mainIngridient = mainIngridient;
+            this.description = description;
+            this.photo = photo;
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
